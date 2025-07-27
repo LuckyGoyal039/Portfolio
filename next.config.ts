8f0310ac98a98326421be82a37e152bc5bf7d559
Lucky <goyallucky2020@gmail.com>
2025-07-27 11:22:28 +0530

    change next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: isProd ? "" : "",
+  basePath: "portfolio",
+  assetPrefix: "portfolio"
 
   /* config options here */
 };
